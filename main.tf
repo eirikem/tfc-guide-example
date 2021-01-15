a3c4e6148992a4c56280478921fb979b118a450e
Eirik Emanuelsen <eirik.emanuelsen@nov.com>
2021-01-15 10:30:11 +0100

    test2

--- main.tf
+++ main.tf
@@ -29,21 +29,21 @@ resource "aws_dynamodb_table" "tfc_example_table" {
   }
 }
 
-resource "aws_dynamodb_table" "tfc_example_table" {
+resource "aws_dynamodb_table" "tfc_example_table2" {
   name = "${var.db_table_name}-${random_pet.table_name.id}"
 
   read_capacity  = var.db_read_capacity
   write_capacity = var.db_write_capacity
-  hash_key       = "UUID"
-  range_key      = "UserName"
+  hash_key       = "UUID2"
+  range_key      = "UserName2"
 
   attribute {
-    name = "UUID"
+    name = "UUID2"
     type = "S"
   }
 
   attribute {
-    name = "UserName"
+    name = "UserName2"
     type = "S"
   }
 }
